86e9bb1acc3455ee6a1fedd2a28bb22419b83c98
MichaelI.Insberg <michael.insberg@4micro.de>
2023-02-04 16:20:08 +0100

    Finished unit tests

--- Core.Contracts/PieceCountCalculator.cs
+++ Core.Contracts/PieceCountCalculator.cs
@@ -1,12 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace PieceCounter.Core.Contracts
+namespace PieceCounter.Core.Contracts;
+
+/// <summary>
+/// The PieceCountCalculator class
+/// </summary>
+public sealed class PieceCountCalculator
 {
-    internal class PieceCountCalculator
+    private readonly IPieceCounterSource pieceCounterSource;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PieceCountCalculator"/> class.
+    /// </summary>
+    /// <param name="pieceCounterSource">The piece counter source.</param>
+    public PieceCountCalculator(IPieceCounterSource pieceCounterSource)
     {
+        this.pieceCounterSource = pieceCounterSource ?? throw new ArgumentNullException(nameof(pieceCounterSource));
+    }
+
+    /// <summary>
+    /// Calculates the piece count.
+    /// </summary>
+    /// <returns>The produced piece count</returns>
+    public int CalculatePieceCount()
+    {
+        var pieces = pieceCounterSource.GetProducedPieces();
+        if (pieces == null)
+        {
+            throw new InvalidOperationException("The piece counter source returned null");
+        }
+
+        var pieceList = pieces.ToList();
+
+        switch (pieceList.Count)
+        {
+            case 0:
+                return 0;
+            case 1:
+                return pieceList[0];
+        }
+
+        var highestValues = new List<int> { 0 };
+
+        foreach (int piece in pieceList)
+        {
+            bool mustAddItem = piece == 0 && highestValues[^1] != 0;
+            if (mustAddItem)
+            {
+                highestValues.Add(0);
+            }
+            highestValues[^1] = piece;
+        }
+
+        return highestValues.Sum();
     }
 }
---
+++ Core.Tests/PieceCountCalculatorTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using PieceCounter.Core.Contracts;
+#pragma warning disable CS1591
+
+namespace PieceCounter.Core.Tests;
+
+[TestFixture]
+public sealed class PieceCountCalculatorTests
+{
+    private Mock<IPieceCounterSource> pieceCounterSourceMock;
+    private PieceCountCalculator objectToTest;
+
+    [SetUp]
+    public void Setup()
+    {
+        pieceCounterSourceMock = new Mock<IPieceCounterSource>();
+        objectToTest = new PieceCountCalculator(pieceCounterSourceMock.Object);
+    }
+
+    [Test]
+    public void CreatePieceCountCalculator_WithParameterNull_ShouldThrow()
+    {
+        //Arrange
+        var func = () => _ = new PieceCountCalculator(null);
+
+        //Act & Assert
+        _ = func.Should().Throw<ArgumentNullException>();
+    }
+
+    [Test]
+    public void CreatePieceCountCalculator_WithPieceCounterSourceReturnsNull_ShouldThrow()
+    {
+        //Arrange
+        _ = pieceCounterSourceMock.Setup(p => p.GetProducedPieces()).Returns((IEnumerable<int>)null);
+        var func = () => _ = objectToTest.CalculatePieceCount();
+
+        //Act & Assert
+        _ = func.Should().Throw<InvalidOperationException>();
+    }
+
+    [TestCaseSource(nameof(TestData))]
+    public void CreatePieceCountCalculator_WithValidInput_ReturnsValidValues(IEnumerable<int> pieces, int expectedResult)
+    {
+        //Arrange
+        _ = pieceCounterSourceMock.Setup(p => p.GetProducedPieces()).Returns(pieces);
+        //Act
+        var result = objectToTest.CalculatePieceCount();
+
+        //Act & Assert
+        _ = result.Should().Be(expectedResult);
+    }
+
+    private static IEnumerable<TestCaseData> TestData()
+    {
+        yield return new TestCaseData(new List<int>(), 0).SetName("Empty list");
+        yield return new TestCaseData(new List<int> { 100 }, 100).SetName("List with only on value");
+        yield return new TestCaseData(new List<int> { 10, 100 }, 100).SetName("List with 2 values");
+        yield return new TestCaseData(new List<int> { 0, 10, 100 }, 100).SetName("List with 3 values start with zero");
+        yield return new TestCaseData(new List<int> { 0, 10, 0 }, 10).SetName("List with 3 values start and ends with zero");
+        yield return new TestCaseData(new List<int> { 0, 10, 0, 20 }, 30).SetName("List with 3 values start and ends with zero");
+        yield return new TestCaseData(new List<int> { 0, 10, 20, 30, 0, 10, 20 }, 50).SetName("List with 3 values start and ends with zero");
+        yield return new TestCaseData(new List<int> { 0, 10, 20, 30, 0, 10, 20, 0, 100 }, 150).SetName("List with 3 values start and ends with zero");
+    }
+}
