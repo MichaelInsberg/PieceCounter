75a040e15d3334ebda9d5165f5f0970a5b913c08
MichaelI.Insberg <michael.insberg@4micro.de>
2023-02-04 14:17:48 +0100

    Add project files.

---
+++ Core.Contracts/IPieceCounterSource.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace PieceCounter.Core.Contracts;
+
+/// <summary>
+/// The IPieceCounterSource interface
+/// </summary>
+public interface IPieceCounterSource
+{
+    /// <summary>
+    /// Gets the produced pieces.
+    /// </summary>
+    /// <returns>An enumerable with the produced pieces</returns>
+    IEnumerable<int> GetProducedPieces();
+
+}
---
+++ Core.Contracts/PieceCountCalculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PieceCounter.Core.Contracts
+{
+    internal class PieceCountCalculator
+    {
+    }
+}
